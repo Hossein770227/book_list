76d9b95d1d23575c4b20e9470e24526e8b2ef233
hossein <Hossein.m.7782@gmail.com>
2023-11-23 14:16:55 +0330

    aaaaaa

--- books/views.py
+++ books/views.py
@@ -18,13 +18,13 @@ class BookDetailView(generic.DetailView):
 
 class BookCreateView(generic.CreateView):
     model=Books
-    fields=['title', 'author', 'description', 'price']
+    fields=['title', 'author', 'description', 'price','cover']
     template_name='books/book_create.html'
 
 
 class BookUpdateView(generic.UpdateView):
     model=Books
-    fields=['title', 'author', 'description', 'price']
+    fields=['title', 'author', 'description', 'price','cover']
     template_name='books/book_update.html'
 
 
